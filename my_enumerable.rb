1204589727b7707445b9904e98e377a23e7bc5e6
Derrick <djmulinde@gmail.com>
2022-12-16 18:34:03 +0300

    Add linters and fix linter errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,21 +1,20 @@
 module MyEnumerable
-  # All?
   def all?
     arr = []
     @list.each { |i| arr.push(i) if yield i }
     puts arr.length == @list.length
   end
-  # any?
+
   def any?
-    arr = []
-    @list.each { |i| arr.push(i) if yield i }
-    puts true ? arr.length > 0 : false
+    @list.each do |i|
+      puts true if yield i
+    end
+    puts false
   end
-  
-  #filter
+
   def filter
     @arr = []
     @list.each { |i| @arr.push(i) if yield i }
     print "#{@arr} \n"
-  end 
+  end
 end
