c8cd9139409bd04ddc6280367f175b90a143aeef
Daron976 <daronkwiafe@gmail.com>
2022-12-16 15:30:44 +0200

    Complete project

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -5,4 +5,17 @@ module MyEnumerable
     @list.each { |i| arr.push(i) if yield i }
     puts arr.length == @list.length
   end
+  # any?
+  def any?
+    arr = []
+    @list.each { |i| arr.push(i) if yield i }
+    puts true ? arr.length > 0 : false
+  end
+  
+  #filter
+  def filter
+    @arr = []
+    @list.each { |i| @arr.push(i) if yield i }
+    print "#{@arr} \n"
+  end 
 end
